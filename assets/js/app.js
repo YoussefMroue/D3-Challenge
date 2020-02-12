db5ab1031c5160797c98fce8587ce5db7675171f
YoussefMroue <41927454+YoussefMroue@users.noreply.github.com>
2020-02-11 18:31:35 -0600

    Finished required portion and began bonus

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
-var svgWidth = 1000;
-var svgHeight = 700;
+var svgWidth = 960;
+var svgHeight = 660;
 
 var margin = {
   top: 20,
@@ -13,8 +13,8 @@ var chartHeight = svgHeight - margin.top - margin.bottom;
 
 var svg = d3.select("#scatter")
   .append("svg")
-  .append("width", svgWidth)
-  .append("height", svgHeight);
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
 
 var scatterGroup = svg.append("g")
   .attr("transform",`translate(${margin.left}, ${margin.top})`);
@@ -24,13 +24,13 @@ d3.csv("assets/data/data.csv").then(function(censusData){
     data.poverty = +data.poverty;
     data.healthcare = +data.healthcare;
     });
-
+    console.log(censusData,d=>d.poverty)
   var xLinearScale = d3.scaleLinear()
-    .domain([20, d3.max(censusData, d=> d.poverty)])
-    .range([0, chartWidth]);
+    .domain([8, d3.max(censusData, d=> d.poverty)])
+    .range([10, chartWidth]);
 
   var yLinearScale = d3.scaleLinear()
-    .domain([0, d3.max(censusData,d => d.healthcare)])
+    .domain([4, d3.max(censusData,d => d.healthcare)])
     .range([chartHeight, 0]);
 
   var bottomAxis = d3.axisBottom(xLinearScale);
@@ -43,13 +43,36 @@ d3.csv("assets/data/data.csv").then(function(censusData){
   scatterGroup.append("g")
     .call(leftAxis)
 
-  var circlesGroup = scatterGroup . selectAll("circle")
+  var circlesGroup = scatterGroup.selectAll("circle")
     .data(censusData)
-    .enter()
-    .append("circle")
+    .enter();
+  
+    circlesGroup.append("circle")
     .attr("cx", d => xLinearScale(d.poverty))
     .attr("cy", d => yLinearScale(d.healthcare))
-    .attr("r","5")
+    .attr("r","12")
     .attr("fill","pink")
-    .attr("opacity","1")
+    .attr("opacity","1");
+    
+    circlesGroup.append("text")
+    .attr("x", d=> xLinearScale(d.poverty))
+    .attr("y", d=> yLinearScale(d.healthcare))
+    .attr("dy","5px")
+    .attr("class","stateText")
+    .text(function(d){
+      return d.abbr;
+    });
+
+  scatterGroup.append("text")
+    .attr("transform","rotate(-90)")
+    .attr("y", 0 - margin.left + 40)
+    .attr("x", 0 - (chartHeight / 2))
+    .attr("dy", "1em")
+    .attr("class", "axisText")
+    .text("In Poverty (%)");
+
+  scatterGroup.append("text")
+    .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + margin.top + 30})`)
+    .attr("class", "axisText")
+    .text("Lacks Healthcare (%)");
   });
